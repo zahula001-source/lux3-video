b4cf4fc6cc975b1520ed9ed6df0d14602c409d1c
minhminh1212 <zahula001@gmail.com>
2026-09-02 02:57:43 +0700

    Fix Downloads: Bat lai accept_downloads=True de Playwright luu file vao thu muc Downloads thay vi huy bo. Watcher se tu dong doi duoi.

--- app/browser.py
+++ app/browser.py
@@ -130,6 +130,8 @@ def get_chromium_runner_simple(profile_id, user_data_dir, proxy_dict, fingerprin
     lines.append('            channel="chrome",')
     lines.append('            ignore_default_args=["--disable-extensions"],')
     lines.append("            args=args,")
+    lines.append('            downloads_path=str(Path.home() / "Downloads"),')
+    lines.append('            accept_downloads=True,')
     lines.append("        )")
     code_no_ext = """
         if proxy:
